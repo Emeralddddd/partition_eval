c20bd8d76aaeeb1beb357a6a5919e02f43a82c7e
Your Name <youd@example.com>
2023-06-15 14:18:38 +0800

    revise merger to load parital results from proto

--- source/merge/merger.cpp
+++ source/merge/merger.cpp
@@ -1,6 +1,7 @@
 #include "merger.hpp"
 #include <algorithm>
 #include <queue>
+#include <fstream>
 
 using std::vector;
 
@@ -18,7 +19,25 @@ void load(std::string path, vector<int>& partition, vector<vector<double>>& prio
     return;
 }
 
-void Merger::init(const std::string& path){
+void load_partial_result(std::string path, PartialResult& pr){
+    std::ifstream in_file(path, std::ios::binary);
+    if (!in_file) {
+        std::cout << "open file error" << std::endl;
+        return;
+    }
+    std::string input((std::istreambuf_iterator<char>(in_file)), std::istreambuf_iterator<char>());
+    if (!in_file) {
+        std::cout << "read file error" << std::endl;
+        return;
+    }
+    if (!pr.ParseFromString(input)) {
+        std::cout << "parse file error" << std::endl;
+        return;
+    }
+}
+
+[[deprecated("no need init")]]
+void Merger::init(const std::string& path) { 
     vector<int> partition;
     vector<vector<double>> priority;
     load(path,partition,priority);
@@ -28,21 +47,44 @@ void Merger::init(const std::string& path){
         assert(priority[i].size() == n_embeds_);
     }
     assert(partition.size() == n_embeds_);
-    weights_.resize(n_embeds_, vector<int>(n_parts_, 0));
-    priority_.resize(n_parts_, vector<double>(n_embeds_,0));
     addPartition(partition,priority);
 }
 
-void Merger::update(const std::string &path){
-    vector<int> partition;
-    vector<vector<double>> priority;
-    load(path,partition,priority);
+void Merger::resize(int n_embeds){
+    n_embeds_ = n_embeds;
+    weights_.resize(n_embeds_, vector<int>(n_parts_));
     for(int i = 0; i < n_parts_; i++){
-        assert(priority[i].size() == n_embeds_);
+        priority_[i].resize(n_embeds_,0);
     }
-    assert(priority.size() == n_parts_);
-    assert(partition.size() == n_embeds_);
-    addPartition(partition,priority);
+}
+
+void Merger::update(const std::string &path){
+    PartialResult pr;
+    load_partial_result(path,pr);
+    assert(pr.priority_maps().size() == n_parts_);
+    addPartition(pr);
+}
+
+void Merger::addPartition(const PartialResult& pr){
+    for(const auto &kv : pr.partition_map()){
+        int key = kv.first;
+        int value = kv.second;
+        if(key >= n_embeds_) resize(key + 1);
+        if(value >= 0){
+            ++weights_[key][value];
+        }
+    }
+    for (int i = 0; i < n_parts_; ++i) {
+        const auto& pm = pr.priority_maps(i);
+        for (const auto& kv : pm.map_field()) {
+            int key = kv.first;
+            float value = kv.second;
+            if(key >= n_embeds_) resize(key + 1);
+            if(value > 0){
+                priority_[i][key] += value;
+            }
+        }
+    }
 }
 
 void Merger::addPartition(const vector<int>& new_partition, \
@@ -60,10 +102,8 @@ void Merger::addPartition(const vector<int>& new_partition, \
     #pragma omp parallel for num_threads(32)
     for(int i = 0; i < n_parts_; i++){
         for(int j = 0; j < n_embeds_;j++){
-            if(j > n_embeds_ - 5) std::cout << new_priority[i][j] << " ";
             priority_[i][j] += std::max(0.,new_priority[i][j]);
         }
-        std::cout << std::endl;
     }
 }
 
@@ -79,7 +119,6 @@ PartitionResult Merger::generatePartition(double hot_rate){
     for(int i = 0; i < n_parts_; i++){
         std::priority_queue<std::pair<double,int>, std::vector<std::pair<double,int>>, std::greater<std::pair<double,int>>> minHeap;
         for(int j = 0; j < n_embeds_; j++){
-            if(j > n_embeds_ - 5) std::cout << priority_[i][j] << " ";
             if(priority_[i][j] <= 0 || ret.partition[j] == i) continue;
             if(minHeap.size() < hot_length) minHeap.emplace(priority_[i][j],j);
             else if(minHeap.top().first < priority_[i][j]){
@@ -87,7 +126,6 @@ PartitionResult Merger::generatePartition(double hot_rate){
                 minHeap.emplace(priority_[i][j],j);
             }
         }
-        std::cout << std::endl;
         for(int j = 0; j < hot_length && !minHeap.empty(); j++){
             ret.caches[i][j] = minHeap.top().second;
             minHeap.pop();
--- source/merge/merger.hpp
+++ source/merge/merger.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "cnpy.h"
 #include <algorithm>
+#include "partial_result.pb.h"
 
 class PartitionResult{
 public:
@@ -10,8 +11,12 @@ public:
 
 class Merger{
 public:
-    Merger() {}
-    Merger(int n_parts) : n_parts_(n_parts) {}
+    Merger() = delete;
+    Merger(int n_parts) : n_parts_(n_parts) {
+        weights_.resize(1, std::vector<int>(n_parts_));
+        priority_.resize(n_parts);
+        caches_.resize(n_parts);
+    }
 
     void init(const std::string& path);
 
@@ -19,6 +24,8 @@ public:
 
     void addPartition(const std::vector<int>& new_partition, \
     const std::vector<std::vector<double>>& new_priority);
+    void addPartition(const PartialResult& pr);
+    void resize(int n_embeds);
     
 
     PartitionResult generatePartition(double hot_rate);
@@ -32,3 +39,5 @@ private:
 
 void load(std::string path, std::vector<int>& partition, \
     std::vector<std::vector<double>>& priority);
+
+void load_partial_result(std::string path, PartialResult& pr);
