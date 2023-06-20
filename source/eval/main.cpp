0d454740c34594b54910fb89a990006373c552d6
Your Name <your.email@example.com>
2023-06-20 20:49:11 +0800

    rewrite param in main eval

--- source/eval/main.cpp
+++ source/eval/main.cpp
@@ -13,6 +13,8 @@
 using std::vector;
 using std::unordered_set;
 
+const static std::string CRITEO_PATH = "/home/xuzhizhen/datasets/criteo-tb/";
+
 int load_data(std::string path, std::vector<std::vector<int>> &data){
   cnpy::NpyArray ndata = cnpy::npy_load(path);
   data.resize(ndata.shape[0], std::vector<int>(ndata.shape[1],-1));
@@ -35,25 +37,30 @@ auto main() -> int
         return 0;
     }
   }
-  const int records_per_day = 20000000;
+  constexpr double HOT_RATE = 0.001;
+  constexpr int PART_NUMS = 4;
   std::vector<std::vector<int>> data;
-  load_data("/data/1/zhen/criteo-tb/sparse_day_0.npy", data);
+  load_data(CRITEO_PATH + "sparse_day_0.npy", data);
   std::cout << "data loaded" << std::endl;
-  StaticPartition partitionHET(4,"/data/1/zhen/criteo-tb/partition/full/day0_1m.npz",0.001);
-  StaticPartition partitionMerge(4,"/data/1/zhen/criteo-tb/partition/full/day0_1m.npz",0.001);
-  // Merger merger(4);
+  StaticPartition partitionHET(PART_NUMS,CRITEO_PATH + "partition/full/day0_1m.npz",HOT_RATE);
+  StaticPartition partitionMerge(PART_NUMS,CRITEO_PATH + "partition/full/day0_1m.npz",HOT_RATE);
+  Merger merger(PART_NUMS);
   vector<double> cost;
   vector<double> mergeCost;
   std::cout << "partition test start" << std::endl;
   auto start_time = std::chrono::system_clock::now();
   auto t = std::chrono::system_clock::now();
   std::unordered_set<int> ban_points = {22,23,24,26,27,28,31,32,33,34,36,39,42,43,51,56};
-  for(int i = 1; i < 80000000; i++){
-    // if(i % 1000000 == 0 && !ban_points.count(i/1000000)){
-    //   // merger.update("/data/1/zhen/criteo-tb/partition/new/day0_" + std::to_string(i/1000000) + "m.bin");
-    //   // partitionMerge.load_partition_from_merger(merger.generatePartition(0.001));
-    //   partitionHET.load_partition_from_npz("/data/1/zhen/criteo-tb/partition/full/day0_"+ std::to_string(i/1000000) +"m.npz",0.001);
-    // } 
+  for(int i = 20000000; i < 80000000; i++){
+    if(i % 1000000 == 0){
+      auto t0 = std::chrono::system_clock::now();
+      merger.update(CRITEO_PATH + "partition/new/day0_" + std::to_string(i/1000000) + "m.bin");
+      partitionMerge.load_partition_from_merger(merger.generatePartition(HOT_RATE));
+      auto t1 = std::chrono::system_clock::now();
+      std::chrono::duration<double> elapsed_time = t1 - t0;
+      std::cout << "Merge time : " << elapsed_time.count();
+      partitionHET.load_partition_from_npz(CRITEO_PATH + "partition/full/day0_"+ std::to_string(i/1000000) +"m.npz",HOT_RATE);
+    } 
     int bs = 1;
     int fieldSize = 26;
     vector<int> currentInput;
@@ -65,7 +72,7 @@ auto main() -> int
       }
     }
     partitionHET.processRequest(currentInput);
-    // partitionMerge.processRequest(currentInput);
+    partitionMerge.processRequest(currentInput);
     if(i > 0 && i%100000 == 0){
       auto now = std::chrono::system_clock::now();
       std::chrono::duration<double> elapsed_time = now - t;
@@ -90,7 +97,7 @@ auto main() -> int
   auto now = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_time = now - start_time;
   std::cout << "partition test time: " << elapsed_time.count() << std::endl;
-  for(int i = 0; i < 4; i++){
+  for(int i = 0; i < PART_NUMS; i++){
     std::cout <<" partition " << i << " Het partition "<< partitionHET.getPartitionCnt(i)\
     << " Het acccess count " << partitionHET.getAccessCnt(i) << std::endl;
   }
