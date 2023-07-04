e679ba9d2c868208d9b7d5e40b4b6e44f8413c1f
Your Name <your.email@example.com>
2023-07-04 09:42:48 +0800

    link embed server & merger

--- scripts/generate_grpc.sh
+++ scripts/generate_grpc.sh
@@ -3,4 +3,4 @@ cd "$SCRIPT_DIR/.."
 
 ./build/bin/protoc -I source/proto --grpc_out=build/source/embed_server --plugin=protoc-gen-grpc=build/bin/grpc_cpp_plugin embed_server.proto inference_request.proto
 ./build/bin/protoc -I source/proto --cpp_out=build/source/embed_server embed_server.proto inference_request.proto
-protoc -I source/proto --cpp_out=build/source/merge partial_result.proto
+./build/bin/protoc -I source/proto --cpp_out=build/source/merge partial_result.proto
